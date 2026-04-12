1597e70122b3ccf94056b37e7334f95cd01c37e5
aobaid <loneaobaid786@gmail.com>
2026-04-12 22:55:19 +0530

    rectangle perimeter

---
+++ perimeter_of_rectangle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+    {
+        int a,b,perimeter;
+        printf("enter length of rectangle \n");
+        scanf("%d",&a);
+        printf("enter breadth of rectangle \n");
+scanf("%d", &b);
+perimeter=2*(a+b);
+printf("perimeter of rectangle is %d",perimeter);
+      }  
