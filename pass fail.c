b306723cd78f9e78c47a74ecfa65b359bb5a4c5c
aobaid <loneaobaid786@gmail.com>
2026-04-18 23:35:41 +0530

    pass fail check

---
+++ pass fail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int marks;
+    printf("enter your marks :");
+    scanf("%d", &marks);
+    if(marks>=33){
+        printf("pass");
+    }
+    else{
+        printf("fail");
+    }
+    }
